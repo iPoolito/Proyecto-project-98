6dcc547c6c61a1849b1753630016b55cbeb2c147
pool <poolortegas@gmail.com>
2024-03-26 13:12:32 -0600

    Adding readline-sync

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,6 @@
 #!/usr/bin/env node
+import getUserName from "../src/cli.js"
+
 console.log("¡Bienvenido a Brain Games!")
+
+getUserName()
---
+++ src/cli.js
@@ -0,0 +1,7 @@
+import readlineSync from 'readline-sync'
+
+export default function getUserName() {
+    const name = readlineSync.question('¿Cuál es tu nombre? ')
+    console.log(`¡Hola, ${name}!`)
+    return name
+}
