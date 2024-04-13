71f03aa50b361eec89de47e9b38e13e12ce3b24d
Pool Ortega <86494456+iPoolito@users.noreply.github.com>
2024-04-13 07:55:16 -0600

    Update progression.js

--- src/progression.js
+++ src/progression.js
@@ -1,25 +1,25 @@
 import { createRounds } from './index.js';
 import { getRandomNumber } from './utils.js';
 
-function getProgression(startStep, step) {
-  const progression = [startStep];
-  let currentValue = startStep;
-  for (let i = 0; i < 10; i += 1) {
-    // eslint-disable-next-line no-unused-vars
-    currentValue += step;
-    progression.push(currentValue);
+const questionMessage = '¿Qué número falta en la progresión?';
+const progressionLength = 8;
+
+function getProgression(startStep, step, hiddenNumIdx) {
+  const progression = [];
+  for (let i = 0; i < progressionLength; i += 1) {
+    const current = startStep + i * step;
+    const element = i === hiddenNumIdx ? '..' : current;
+    progression.push(element);
   }
-  const randomIndex = Math.floor(Math.random() * progression.length);
-  const correctAnswer = progression[randomIndex];
-  progression[randomIndex] = '..';
-  return { correctAnswer, progression: progression.join(' ') };
+  return progression.join(' ');
 }
 
 const getRound = () => {
   const startStep = getRandomNumber(100);
   const stepProgression = getRandomNumber(10);
-  const { progression, correctAnswer } = getProgression(startStep, stepProgression);
-  const questionMessage = '¿Qué número falta en la progresión?';
+  const hiddenNumIdx = Math.floor(Math.random() * progressionLength);
+  const progression = getProgression(startStep, stepProgression, hiddenNumIdx);
+  const correctAnswer = startStep + hiddenNumIdx * stepProgression;
   const questionValue = `${progression}`;
   return {
     questionMessage, correctAnswer, questionValue, isNumberAnswer: true,
