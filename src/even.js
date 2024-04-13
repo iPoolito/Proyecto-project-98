49c706004392ba0eaaa2cc28337b22a14546761b
Pool Ortega <86494456+iPoolito@users.noreply.github.com>
2024-04-13 07:54:26 -0600

    Update even.js

--- src/even.js
+++ src/even.js
@@ -1,17 +1,19 @@
 import { createRounds } from './index.js';
 import { getRandomNumber } from './utils.js';
 
+const questionMessage = 'Responde "yes" si el número es par, de lo contrario responde "no".';
+
 function isEven(number) {
   if (number % 2 === 0) {
-    return 'yes';
+    return true;
   }
-  return 'no';
+  return false;
 }
 
 const getRound = () => {
   const randomNumber = getRandomNumber(15);
-  const correctAnswer = isEven(randomNumber);
-  const questionMessage = 'Responde "yes" si el número es par, de lo contrario responde "no".';
+  const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
+
   return { questionMessage, correctAnswer, questionValue: randomNumber };
 };
 
