43d3e9bc23a3050539fe247350f05c7e2f2462c5
Pool Ortega <86494456+iPoolito@users.noreply.github.com>
2024-04-16 17:06:17 -0600

    Update prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,17 +4,17 @@ import getRandomNumber from '../utils.js';
 const questionMessage = 'Responde "yes" si el número dado es primo. De lo contrario, responde "no".';
 
 // Reference that recursive function can be implement
-function isPrimeRecursive(number, divider = 3) {
+function isPrimeNumber(number, divider = 3) {
   if (number === 2) return true;
   if (number < 2 || number % 2 === 0) return false;
   if (divider * divider > number) return true;
   if (number % divider === 0) return false;
-  return isPrimeRecursive(number, divider + 2);
+  return isPrimeNumber(number, divider + 2);
 }
 
 const getRound = () => {
   const randomNumber = getRandomNumber(10);
-  const correctAnswer = isPrimeRecursive(randomNumber) ? 'yes' : 'no';
+  const correctAnswer = isPrimeNumber(randomNumber) ? 'yes' : 'no';
   return { questionMessage, correctAnswer, questionValue: randomNumber };
 };
 
